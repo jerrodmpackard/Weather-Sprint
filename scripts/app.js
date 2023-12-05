e3d2ee3b2cea6f3cf46c1d4c685101786aaa4cf0
jerrodmpackard <jerrodmpackard@gmail.com>
2023-12-05 15:09:34 -0800

    added geolocation

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,9 @@ let visibility = document.getElementById("visibility");
 let windSpeed = document.getElementById("windSpeed");
 let locationCode = document.getElementById("locationCode");
 
+let lat;
+let lon;
+
 // geolocation
 navigator.geolocation.getCurrentPosition(success, errorFunc);
 
@@ -18,34 +21,51 @@ function success(position){
     console.log("Our latitude: " + position.coords.latitude);
     console.log("Our longitude: " + position.coords.longitude);
 
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+
+    currentWeather.innerText = "Locating..."
+
+    currentGeoWeatherCall();
 }
 
 function errorFunc(error){
     console.log(error.message);
 }
 
-// let lat = position.coords.latitude;
-// let lon = position.coords.longitude;
-
-// let lat = -121.2908;
-// let lon = 37.9577;
-
+// Geolocation function -> Current weather API call
 async function currentGeoWeatherCall() {
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
 
     const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`);
 
     const data = await promise.json();
 
-    console.log(data);
+    console.log(`Current weather: ${Math.round(data.main.temp)}° F`);
+    currentWeather.innerText = `Current weather: ${Math.round(data.main.temp)}° F`;
+
+    console.log(`Currently: ${data.weather[0].main}`);
+    currentWeatherMain.innerText = `Currently: ${data.weather[0].main}`;
+
+    console.log(`Currently: ${data.weather[0].description}`);
+    currentWeatherConditions.innerText = `Currently: ${data.weather[0].description}`;
+
+    console.log(`Feels like: ${Math.round(data.main.feels_like)}° F`);
+    feelsLike.innerText = `Feels like: ${Math.round(data.main.feels_like)}° F`;
+
+    console.log(`Humidity: ${Math.round(data.main.humidity)}%`);
+    humidity.innerText = `Humidity: ${Math.round(data.main.humidity)}%`;
+
+    console.log(`Visibility: ${Math.round(data.visibility)}m`);
+    visibility.innerText = `Visibility: ${Math.round(data.visibility)}m`;
+
+    console.log(`Wind Speed: ${Math.round(data.wind.speed)} mph`);
+    windSpeed.innerText = `Wind Speed: ${Math.round(data.wind.speed)} mph`;
+
+    console.log(`Location: ${data.name}, ${data.sys.country}`);
+    locationCode.innerText = `Location: ${data.name}, ${data.sys.country}`;
 }
 
-currentGeoWeatherCall();
-
-// Current weather API call
+// Search function -> Current weather API call
 async function currentWeatherCall() {
     let cityName = cityInput.value.toLowerCase();
 
@@ -76,8 +96,6 @@ async function currentWeatherCall() {
 
     console.log(`Location: ${data.name}, ${data.sys.country}`);
     locationCode.innerText = `Location: ${data.name}, ${data.sys.country}`;
-
-
 }
 
 searchBtn.addEventListener("click", function() {
